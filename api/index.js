340cdfd6f2b9bca0f2f673e37e3341f70bdcbc4f
aw478882609-ops <aw478882609@gmail.com>
2025-09-13 01:26:22 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,6 +28,11 @@ module.exports = async (req, res) => {
             const userId = message.from.id;
             const fileId = message.document.file_id;
 
+            const VERCEL_LIMIT_BYTES = 4 * 1024 * 1024; // 4MB
+    if (message.document.file_size > VERCEL_LIMIT_BYTES) {
+        await bot.sendMessage(chatId, `⚠️ عذرًا، حجم الملف يتجاوز الحد المسموح به (${'4MB'}). يرجى إرسال ملف أصغر.`);
+        return res.status(200).send('OK');
+    }
             if (message.document.mime_type !== 'application/pdf') {
                 await bot.sendMessage(chatId, '⚠️ يرجى إرسال ملف بصيغة PDF فقط.');
                 return res.status(200).send('OK');
